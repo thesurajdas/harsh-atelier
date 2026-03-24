1247f5bc9d717a100cfc6552af6550b750c84447
Suraj Das <suraj4apps@gmail.com>
2026-03-25 00:14:50 +0530

    fix scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import pantherLogo from '../assets/home.png'
 const Home = ({ onContactClick }) => {
   return (
     <section className="section landing">
-     <img src={pantherLogo} alt="Logo" style={{ width: '370px', marginBottom: '1rem' }} />
+      <img src={pantherLogo} alt="Logo" className="home-logo" />
       <div style={{ marginTop: '3rem', fontFamily: 'var(--font-serif)', opacity: '100%' }}>scroll down to move right &rarr;</div>
       <div className="contact-button" onClick={onContactClick}>Contact us</div>
     </section>
